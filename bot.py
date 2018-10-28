32fa2971f5ef8a9b54ae4799adaad8a194d1e27b
I-am-Gabi <gabicavalcantesilva@gmail.com>
2018-10-28 01:46:10 -0100

    capture replay msg

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import configparser
 from telebot import TeleBot, types
+from pprint import pprint
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -8,7 +9,7 @@ config.read('config.ini')
 token = config['DEFAULT']['BOT_API_TOKEN']
 bot = TeleBot(token) 
 
-safe_users = []
+safe_users = set()
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
@@ -17,7 +18,7 @@ def send_welcome(message):
     bot.reply_to(message, msg)  
 
 def gen_markup(one_time_keyboard=True, resize_keyboard=True, selective=False):
-    markup = types.ForceReply(one_time_keyboard=one_time_keyboard, 
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=one_time_keyboard, 
     resize_keyboard=resize_keyboard, selective=selective)
     markup.add(types.KeyboardButton("Sim!"))
     return markup 
@@ -25,32 +26,47 @@ def gen_markup(one_time_keyboard=True, resize_keyboard=True, selective=False):
 @bot.message_handler(commands=['check'])
 def message_handler(message):
     if (len(safe_users) > 0):
-        if (message.from_user.username not in safe_users):
+        if (message.from_user.id not in safe_users):
             msg = "@{}, você está safe?".format(message.from_user.username) 
             response = bot.send_message(message.chat.id, msg, reply_markup=gen_markup(selective=True))
     else:
         msg = "Você está safe?"
         response = bot.send_message(message.chat.id, msg, reply_markup=gen_markup())
     
-    bot.register_next_step_handler(response, process_response)
+    # bot.register_next_step_handler(response, process_response)
 
 @bot.message_handler(commands=['safes'])
 def message_handler_safes(message):
-    try:
-        bot.reply_to(message, ', '.join(safe_users))  
+    try:  
+        users = []
+        print(safe_users)
+        for user_id in safe_users:  
+            #print(user_id, bot.get_chat_member(message.chat.id, user_id))
+            users.append(bot.get_chat_member(message.chat.id, user_id).user.first_name)
+        bot.reply_to(message, ', '.join(users))  
     except Exception as e:
         bot.reply_to(message, 'ops, ninguém chegou :(')
 
 @bot.message_handler(commands=['clean'])
 def message_handler_clean(message):
-    safe_users = []
+    safe_users = set()
 
 def process_response(message):
     try:
         chat_id = message.chat.id   
-        safe_users.append(message.from_user.username)  
-        bot.send_message(chat_id, 'Ufa, ' + message.from_user.username)
+        safe_users.add(chat_id)  
+        bot.send_message(chat_id, 'Ufa, ' + message.from_user.first_name)
     except Exception as e:
         bot.reply_to(message, 'oooops')
-     
+
+@bot.message_handler(func=lambda message: message.text == 'Sim!' and message.content_type == 'text')
+def message_handler_response(message):
+    try:  
+        chat_id = message.chat.id  
+        user_id = message.from_user.id
+        safe_users.add(user_id)  
+        bot.send_message(chat_id, 'Ufa, ' + message.from_user.first_name)
+    except Exception as e: 
+        bot.reply_to(message, 'oooops')
+
 bot.polling(none_stop=True)
