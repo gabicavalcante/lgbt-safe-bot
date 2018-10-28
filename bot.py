1e64ba6b08348b9d37017dcf997f472b0c50b7f1
I-am-Gabi <gabicavalcantesilva@gmail.com>
2018-10-28 00:22:48 +0000

    add config template

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import configparser
 from telebot import TeleBot, types
 
@@ -12,19 +13,25 @@ safe_users = []
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     msg = """Olá, bem-vindo ao lgbt safe!
-    Este grupo foi feito para garantir nossa segurança dentro da universidade.
-    Vamos evitar sair sozinhos pelo campus."""
+    Este grupo foi feito para garantir nossa segurança dentro da universidade."""
     bot.reply_to(message, msg)  
 
-def gen_markup():
-    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+def gen_markup(one_time_keyboard=True, resize_keyboard=True, selective=False):
+    markup = types.ForceReply(one_time_keyboard=one_time_keyboard, 
+    resize_keyboard=resize_keyboard, selective=selective)
     markup.add(types.KeyboardButton("Sim!"))
     return markup 
 
 @bot.message_handler(commands=['check'])
 def message_handler(message):
-    msg = "Você está safe?".format(message.from_user.username) 
-    response = bot.send_message(message.chat.id, msg, reply_markup=gen_markup())
+    if (len(safe_users) > 0):
+        if (message.from_user.username not in safe_users):
+            msg = "@{}, você está safe?".format(message.from_user.username) 
+            response = bot.send_message(message.chat.id, msg, reply_markup=gen_markup(selective=True))
+    else:
+        msg = "Você está safe?"
+        response = bot.send_message(message.chat.id, msg, reply_markup=gen_markup())
+    
     bot.register_next_step_handler(response, process_response)
 
 @bot.message_handler(commands=['safes'])
@@ -33,6 +40,10 @@ def message_handler_safes(message):
         bot.reply_to(message, ', '.join(safe_users))  
     except Exception as e:
         bot.reply_to(message, 'ops, ninguém chegou :(')
+
+@bot.message_handler(commands=['clean'])
+def message_handler_clean(message):
+    safe_users = []
 
 def process_response(message):
     try:
